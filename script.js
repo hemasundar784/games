b24c93d2b3d80b1ff529dfc577b2c8bb66ce3b4b
hemasundar784 <hemasundar784@gmail.com>
2026-06-05 02:37:34 +0530

    sound change

--- script.js
+++ script.js
@@ -197,7 +197,7 @@ function triggerCombatFX(attackerToken) {
         case 'P': strikeText = "🐾 MEOW KILL! 🐾"; textStyleClass = "text-cat"; break;
         case 'N': strikeText = "✨ MYSTICAL STRIKE! ✨"; textStyleClass = "text-unicorn"; break;
         case 'B': strikeText = "🦁 Cunning Cut! 🦁"; textStyleClass = "text-lion"; break;
-        case 'R': strikeText = "🐯 Rooring Bullll 🐯"; textStyleClass = "text-tiger"; scratchMarks.classList.add('slash-active'); break;
+        case 'R': strikeText = "🐯 Roaring Bullll 🐯"; textStyleClass = "text-tiger"; scratchMarks.classList.add('slash-active'); break;
         case 'Q': strikeText = "🦊 SLY DECEPTION! 🦊"; textStyleClass = "text-fox"; break;
         case 'K': strikeText = "👑 ROYAL EXECUTION! 👑"; textStyleClass = "text-king"; break;
         case 'p': strikeText = "🐾 MEOW KILL! 🐾"; textStyleClass = "text-cat"; break;
